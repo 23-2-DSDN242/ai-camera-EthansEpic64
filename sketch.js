e948bda5868d6a65ee64b078b7525e952b5383b3
EthansEpic64 <131923976+EthansEpic64@users.noreply.github.com>
2024-05-28 11:05:36 +1200

    stgssggssdgd

--- sketch.js
+++ sketch.js
@@ -25,69 +25,78 @@ function setup () {
   sourceImg.loadPixels();
   facemaskImg.loadPixels();
   carmaskImg.loadPixels();
-  colorMode(HSB);
 }
 
-let X_STOP = 640;
-let Y_STOP = 480;
-let OFFSET = 6;
+// let X_STOP = 640;
+// let Y_STOP = 480;
 
-let renderCounter=5;
+let X_STOP = 1920;
+let Y_STOP = 1080;
+
+let OFFSET = 2;
+
+let renderCounter=0;
 
 function draw () {
-  //let x = floor(random(sourceImg.width));
-  //let y = floor(random(sourceImg.height));
-  //let pix = sourceImg.get(x, y);
-  //let car = carmaskImg.get(x, y);
-  //let face = facemaskImg.get(x,y);
-  let num_lines_to_draw = 40;
-  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
-    for(let i=5; i<X_STOP; i++) {
-      colorMode(RGB);
-      let pix = [0, 0, 0, 255];
-      let car = carmaskImg.get(i, j);
-      if (car[1] > 128) {
-        pix = sourceImg.get(i, j);
-      }
-      else {
-        let sum_rgb = [0, 0, 0]
-        let num_cells = 0;
-        for(let wx=-OFFSET;wx<OFFSET;wx++){
-          for (let wy=-OFFSET;wy<OFFSET;wy++) {
-            let pix = sourceImg.get(i+wx, j+wy);
-            for(let c=0; c<3; c++) {
-              sum_rgb[c] += pix[c];
-            }
-            num_cells += 1;
-          }
-        }
-        for(let c=0; c<3; c++) {
-          pix[c] = int(sum_rgb[c] / num_cells);
-        }        
-      }
 
-      set(i, j, pix);
+  // colorMode(HSB);
+  // let num_lines_to_draw = 20;
+  // for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+  //   for(let i=5; i<X_STOP; i++) {
+  //     colorMode(RGB);
+  //     let pix = [0, 0, 0, 255];
+  //     let car = carmaskImg.get(i, j);
+  //     if (car[1] > 128) {
+  //       pix = sourceImg.get(i, j);
+  //     }
+  //     else {
+  //       let sum_rgb = [0, 0, 0]
+  //       let num_cells = 0;
+  //       for(let wx=-OFFSET;wx<OFFSET;wx++){
+  //         for (let wy=-OFFSET;wy<OFFSET;wy++) {
+  //           let pix = sourceImg.get(i+wx, j+wy);
+  //           for(let c=0; c<3; c++) {
+  //             sum_rgb[c] += pix[c];
+  //           }
+  //           num_cells += 1;
+  //         }
+  //       }
+  //       for(let c=0; c<3; c++) {
+  //         pix[c] = int(sum_rgb[c] / num_cells);
+  //       }        
+  //    }
+
+  //    set(i, j, pix);
+  // }
+  // }
+ 
+  colorMode(RGB);
+  for(let i=0;i<4000;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix2 = sourceImg.get(x, y);
+    let car2 = carmaskImg.get(x, y);
+    let face2 = facemaskImg.get(x,y);
+  
+    fill(pix2);
+    if(face2[0] > 128) {
+      let pointSize = 20;
+      fill(255,0,0,80);
+      rect(x, y, pointSize, pointSize);
     }
-  //for(let i=0;i<4000;i++) {
-    
-    
-    //fill(pix);
-    //if(face[0] > 128) {
-     // let pointSize = 20;
-     // fill(255,0,0,80);
-     // rect(x, y, pointSize, pointSize);
-   // }
-    //else {
-    //  let pointSize = 10;
-     // rect(x, y, pointSize, pointSize);    
-   // }
+    else {
+      let pointSize = 10;
+      rect(x, y, pointSize, pointSize);    
+    }
 
-  //  if(car[0] > 128) {
-    //  let pointSize = 12;
-    //  fill(0);
-    //  rect(x,y,pointSize, pointSize);
-    //}
+    if(car2[0] > 128) {
+      let pointSize = 12;
+      fill(0);
+      rect(x,y,pointSize, pointSize);
+    }
   }
+
+
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
 
@@ -105,3 +114,4 @@ function keyTyped() {
     saveBlocksImages();
   }
 }
+
